364469d6c4938fde91414269c82f01b21d1eb043
CAMARA Pascal <camara@MacBook-Pro-de-CAMARA.local>
2017-01-10 18:03:11 +0100

    < 9 Security

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
  # get "welcome/index"
-  root "welcome#index"
+ # root "welcome#index"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+ end
  
   root 'welcome#index'
 end
